6b1df7687acfc71e07e7469282dcb483c9306d1b
ChantelRaja <55223546+ChantelRaja@users.noreply.github.com>
2021-10-15 10:08:32 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var vis1 = "Vis1.json";
+vegaEmbed("#map",vis1,{"actions":false}).then(function(result){}).catch(console.error);
+
+var vis2 = "Vis2.json";
+vegaEmbed("#bar",vis2,{"actions":false}).then(function(result){}).catch(console.error);
+
+var vis3 = "Vis3.json";
+vegaEmbed("#heat",vis3,{"actions":false}).then(function(result){}).catch(console.error);
